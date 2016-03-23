c0fb90850cf9cd5b9c5aeef0ed489c43e803b582
Sebastien Damaye <sebastien.damaye@gmail.com>
2016-03-23 14:42:16 +0100

    updated origin-cert.sh

--- origin-cert.sh
+++ origin-cert.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-"""
-script adapted from following source:
-https://bugs.winehq.org/show_bug.cgi?id=35828
-"""
-if [[ $EUID -ne 0 ]]; then
-	echo "You need to be root to run this script."
-	exit 1
+#script adapted from following source:
+#https://bugs.winehq.org/show_bug.cgi?id=35828
+
+if kdesudo 2>/dev/null; then
+    gsu="kdesudo"
+else
+    gsu="gksudo"
 fi
 
-wget -qO /tmp/gte-global.zip https://bugs.winehq.org/attachment.cgi?id=51579
-unzip /tmp/gte-global.zip
-mkdir -p /usr/local/share/ca-certificates
-cp /tmp/gte-global.pem /usr/local/share/ca-certificates/gte-global.crt
-update-ca-certificates
-echo "Certificat installé. Relancer Origin"
+cd /tmp/
+wget -qO gte-global.zip https://bugs.winehq.org/attachment.cgi?id=51579
+unzip -o gte-global.zip
+`$gsu "sh -c 'mkdir -p /usr/local/share/ca-certificates;cp /tmp/gte-global/gte-global.pem /usr/local/share/ca-certificates/gte-global.crt;/usr/sbin/update-ca-certificates'"`
+notify-send "Terminé" "Certificat installé. Relancer Origin"
